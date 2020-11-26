d9c715b1d79abfa256f53238b67880f44390af27
SuhailArshad1122 <suhail_a@hotmail.com>
2020-11-26 11:29:50 +0500

    added theme-ui

--- packages/www/src/pages/index.js
+++ packages/www/src/pages/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import { Button, Container, Flex, Heading } from 'theme-ui';
 
 export default props => (
-    <div>
-        AOA My World
-    </div>    
+    <Container>
+        <Flex sx={{ flexDirection: "column", padding: 3}}>
+            <Heading as="h1">AOA My World</Heading> 
+            <Button sx={{marginTop: 2, color: 'black'}}
+                onClick={ ()=> alert("Login Please")}
+            >
+                Click to Add Task
+            </Button>
+        </Flex>
+    </Container>    
 );
